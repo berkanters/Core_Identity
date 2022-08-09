cc66467fd3ee2d03e82bd830140c181c8c57bf34
Kazım Berkant Ersoy <berkante@smartcv.com.tr>
2022-08-09 21:13:08 +0300

    Add project files.

---
+++ CoreIdentity102/ViewModel/RoleViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoreIdentity102.ViewModel
+{
+    public class RoleViewModel
+    {
+        public string Id { get; set; }
+        [Required(ErrorMessage = "{0} Role name is required"), DisplayName("Role Name")]
+
+        public string Name { get; set; }
+    }
+}
